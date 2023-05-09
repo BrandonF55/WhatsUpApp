9dcb98ec211283ba4b045f4e6ad10cbf627b5cb7
Brandonf55 <brandon.fenk99@gmail.com>
2023-05-09 14:39:12 -0700

    Added styling to the input boxes and icons to them as well. In the process of making a login screen for the application.

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,21 +1,43 @@
+
 import { View, StyleSheet, Text, TextInput } from "react-native"
+import { FontAwesome } from '@expo/vector-icons';
 import colors from "../constants/colors";
 
 const Input = props => {
     return (
         <View style={styles.container}>
-           <Text>{props.label}</Text>
+            <Text style={styles.label} >{props.label}</Text>
 
-           <View style={styles.inputContainer}>
-            <TextInput/>
-           </View>
+            <View style={styles.inputContainer}>
+
+                {
+                    props.icon && <props.iconPack
+                        //  as long as the icon property exist it will show a icon and if it doesn't it wont
+                        name={props.icon}
+                        size={props.iconSize || 15}
+                        style={styles.icon}
+
+                    />
+
+                }
+                <TextInput styles={styles.input} />
+            </View>
+
+            {
+                    props.errorText &&
+                 <View style={styles.errorContainer}>
+                 <Text style={styles.errorText}>{props.errorText}</Text>
+             </View>
+
+            }
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-   width: '100%'
+        width: '100%'
     },
     inputContainer: {
         width: '100%',
@@ -23,9 +45,40 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         paddingVertical: 15,
         borderRadius: 2,
-        backgroundColor: colors.nearlyWhite
-    
+        backgroundColor: colors.nearlyWhite,
+        flexDirection: 'row'
+    },
+    icon: {
+        marginRight: 10,
+        color: colors.grey
+    },
+    label: {
+        marginVertical: 8,
+        fontFamily: 'bold',
+        letterSpacing: 0.3,
+        color: colors.textColor
+    },
+    input: {
+        color: colors.textColor,
+        flex: 1,
+        fontFamily: 'regular',
+        letterSpacing: 0.3,
+        paddingTop: 0
+    },
+    errorContainer: {
+        marginVertical: 5
+
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 13,
+        fontFamily: 'regular',
+        letterSpacing: 0.3
+
     }
+
+
+
 });
 
 export default Input;
--- screens/AuthScreen.jsx
+++ screens/AuthScreen.jsx
@@ -3,12 +3,34 @@ import { View, Text, StyleSheet } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 import Input from "../components/Input";
 import PageContainer from "../components/PageContainer";
+import { Feather, FontAwesome } from "@expo/vector-icons";
 
 const AuthScreen = props => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <PageContainer>
-           <Input label='First Name' />
+                <Input
+                    label='First Name'
+                    icon='user-circle-o'
+                    iconPack={FontAwesome}
+                />
+
+                <Input
+                    label='Last Name'
+                    icon='user-circle-o'
+                    iconPack={FontAwesome}
+                />
+                <Input
+                    label='Email'
+                    icon='mail'
+                    iconPack={Feather}
+                />
+
+                <Input
+                    label='Password'
+                    icon='lock'
+                    iconPack={FontAwesome}
+                />
             </PageContainer>
         </SafeAreaView>
     );
