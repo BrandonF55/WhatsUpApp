fde894f084d4585fd4792474166de1fa6126efde
Brandonf55 <brandon.fenk99@gmail.com>
2023-06-05 15:18:36 -0700

    Now you can successfully add users to the database on Firebase. I had a little issue storin users at first but resolved the issue. I was missing password parameter

--- Utils/actions/authActions.jsx
+++ Utils/actions/authActions.jsx
@@ -1,15 +1,23 @@
 
 import { getFirebaseApp } from  '../firebaseHelper';
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { getDatabase, set, child, ref } from  'firebase/database';
+import { async } from 'validate.js';
 
 export const signUp = async (Firstname, Lastname, Email, Password) => {
 
     const app = getFirebaseApp();
     const auth = getAuth(app);
 
+    
+
     try {
         const result = await createUserWithEmailAndPassword(auth, Email, Password);
-        console.log(result)
+        const { uid } = result.user;
+
+        const userData = await createUser(Firstname, Lastname, Email, Password, uid)
+
+        console.log(userData)
     } catch (error) {
         const errorCode = error.code;
 
@@ -21,4 +29,24 @@ export const signUp = async (Firstname, Lastname, Email, Password) => {
 
         throw new Error(message);
     }
+}
+
+const createUser = async (Firstname, Lastname, Email, Password, userId) => {
+    const firstLast = `${Firstname} ${Lastname}`.toLowerCase();
+    // a useful way to be able to search for first and last names with a space between.
+    const userData = {
+        Firstname,
+        Lastname,
+        firstLast,
+        Email,
+        userId,
+        signUpDate: new Date().toISOString()
+        //  toISOString() this is a standard date formatter. 
+    }
+
+    const dbRef = ref(getDatabase());
+    const childRef = child(dbRef,`users/${userId}`)
+    await set(childRef, userData)
+    return userData;
+    //  this is our reference from our database. The real time database on firebase.
 }
