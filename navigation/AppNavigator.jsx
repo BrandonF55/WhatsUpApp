781f41b76e3d6473e4156ab3ee8a92ce31661e8b
Brandonf55 <brandon.fenk99@gmail.com>
2023-06-15 15:04:59 -0700

    Just sloved my issued. I was Handling when the storage is empty in the program. For some reason i could not get off the loading screen. I finally figured it out, it was cased by having capitals instead of lowercase.

--- navigation/AppNavigator.jsx
+++ navigation/AppNavigator.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 
+import MainNavigator from "./MainNavigator";
+import AuthScreen from "../screens/AuthScreen";
+import { useSelector } from "react-redux";
+import StartUpScreen from "../screens/StartUpScreen";
 
-import MainNavigator from './MainNavigator';
-import AuthScreen from '../screens/AuthScreen';
-import { useSelector } from 'react-redux';
+const AppNavigator = (props) => {
 
-
-const AppNavigator = () => {
-
-const isAuth = useSelector(state => state.auth.token !== null && state.auth.token !== "");
+  const isAuth = useSelector(state => state.auth.token !== null && state.auth.token !== "");
+  const didTryAutoLogin = useSelector(state => state.auth.didTryAutoLogin);
 
   return (
     <NavigationContainer>
-
-      {isAuth && <MainNavigator/>}
-      {!isAuth && <AuthScreen/>}
-
+      {isAuth && <MainNavigator />}
+      {!isAuth && didTryAutoLogin && <AuthScreen />}
+      {!isAuth && !didTryAutoLogin && <StartUpScreen />}
     </NavigationContainer>
   );
 };
