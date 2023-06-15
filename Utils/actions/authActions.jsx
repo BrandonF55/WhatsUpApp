17d74d84060e8dfc6b5a9a497e67df9831456ccd
Brandonf55 <brandon.fenk99@gmail.com>
2023-06-15 13:41:30 -0700

    Added the AyncStorage @react-native-async-storage/async-storage to save the user if they refresh the page. It will keep them sighed in instead of taking you back to the sign up form after you refresh the app.

--- Utils/actions/authActions.jsx
+++ Utils/actions/authActions.jsx
@@ -4,6 +4,7 @@ import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import { getDatabase, set, child, ref } from 'firebase/database';
 import { async } from 'validate.js';
 import { authenticate } from '../../store/authSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const signUp = (Firstname, Lastname, Email, Password) => {
     return async dispatch => {
@@ -13,11 +14,14 @@ export const signUp = (Firstname, Lastname, Email, Password) => {
         try {
             const result = await createUserWithEmailAndPassword(auth, Email, Password);
             const { uid, stsTokenManager } = result.user;
-            const { accessToken } = stsTokenManager
+            const { accessToken, expirationTime  } = stsTokenManager
+
+            const expiryDate = new Date(expirationTime)
 
             const userData = await createUser(Firstname, Lastname, Email, Password, uid)
 
             dispatch(authenticate({token: accessToken, userData}));
+            saveDataToStorage(accessToken, uid, expiryDate);
         } catch (error) {
             const errorCode = error.code;
 
@@ -54,4 +58,14 @@ const createUser = async (Firstname, Lastname, Email, Password, userId) => {
     await set(childRef, userData)
     return userData;
     //  this is our reference from our database. The real time database on firebase.
+}
+
+const saveDataToStorage = (token, userId, expiryDate) => {
+    AsyncStorage.setItem("userData", JSON.stringify({
+        token,
+        userId,
+        expiryDate: expiryDate.toISOString()
+        // the json stringify turns a object into a string so it can be stored in the data base.
+
+    }));
 }
